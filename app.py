2dbad0b1efa207c6c00148b304b7905c3b219050
Nellie Tobey <35578867+nelliesnoodles@users.noreply.github.com>
2020-08-19 10:33:10 -0400

    <feature>Color & Contrast
    Adding page: Color and Contrast
    skeleton: preliminary

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,19 @@ def hello_world():
 def index():
     return render_template('SAS_index.html')
 
+@app.route('/color', methods=['GET', 'POST'])
+def color():
+
+    if request.method == 'GET':
+        return render_template('color.html')
+    if request.method == 'POST':
+        pass
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        #return redirect(url_for('index'))
+
+    # redirect to home
+    return render_template('SAS_index.html')
 
 if __name__ == "__main__":
   app.run(debug=True)
